9b25dafe6fd5878944ffac45cbae93781cccf690
ignisf <bordjukov@gmail.com>
2013-07-16 05:50:57 +0300

    Explode the configuration

---
+++ .emacs.d/elisp/init-newline.el
@@ -0,0 +1,17 @@
+;; Opening new lines can be finicky.
+
+(defun open-line-below ()
+  (interactive)
+  (end-of-line)
+  (newline)
+  (indent-for-tab-command))
+
+(defun open-line-above ()
+  (interactive)
+  (beginning-of-line)
+  (newline)
+  (forward-line -1)
+  (indent-for-tab-command))
+
+(global-set-key (kbd "<C-return>") 'open-line-below)
+(global-set-key (kbd "<C-S-return>") 'open-line-above)
