49a6f8ab1e2bc4cb722db92462b2b12c4f6b0c49
ignisf <bordjukov@gmail.com>
2013-07-25 15:10:19 +0300

    Add eimp

---
+++ .emacs.d/elpa/eimp-20091025.922/eimp-autoloads.el
@@ -0,0 +1,28 @@
+;;; eimp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (eimp-mode) "eimp" "eimp.el" (20970 39085 41996
+;;;;;;  697000))
+;;; Generated autoloads from eimp.el
+
+(autoload 'eimp-mode "eimp" "\
+Toggle Eimp mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("eimp-pkg.el") (20970 39085 186705 835000))
+
+;;;***
+
+(provide 'eimp-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; eimp-autoloads.el ends here
