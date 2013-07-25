2590455d9a9781aec507537c6901f7bf42449ca2
ignisf <bordjukov@gmail.com>
2013-07-25 15:10:01 +0300

    Add multiple-cursors.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,7 +15,7 @@
 		       "haml"
 		       "hexcolour"
 		       "magit"
-		       "mark-multiple"
+		       "multiple-cursors"
 		       "newline"
 		       "rspec"
 		       "rainbow"
@@ -26,18 +26,8 @@
 		       "smooth-scroll"
 		       "style"
 		       "twit"
+		       "windows"
+		       "wl"
 		       "yaml"
 		       "yasnippet"))
   (load (format "%s%s%s" "init-" element ".el")))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("dc758223066a28f3c6ef6c42c9136bf4c913ec6d3b710794252dc072a3b92b14" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
