ab4cebb4253c3cf394201542b05a8ab68a662a74
ignisf <bordjukov@gmail.com>
2013-08-09 00:51:36 +0300

    Add a bunch of files that should be recognised as ruby

---
+++ .emacs.d/elisp/init-ruby.el
@@ -0,0 +1,7 @@
+;; Rake files are ruby, too, as are gemspecs, rackup files, and gemfiles.
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
