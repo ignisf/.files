f21926037a93608541defaf6744ec4881fad826a
Petko Bordjukov <petko.bordjukov@shtrak.bg>
2013-11-25 22:16:54 +0200

    Add a submodule for yasnippet

--- .emacs.d/elisp/init-yasnippet.el
+++ .emacs.d/elisp/init-yasnippet.el
@@ -1,1 +1,5 @@
+(add-to-list 'load-path
+              "~/.emacs.d/vendor/yasnippet")
+(require 'yasnippet)
+(setq yas-snippet-dirs '("~/.emacs.d/vendor/yasnippet/extras/imported"))
 (yas-global-mode 1)
