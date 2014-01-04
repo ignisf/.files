e55b8380283fd9cd079e9429a00c44d26bcf3b0d
Petko Bordjukov <bordjukov@gmail.com>
2014-01-05 01:33:19 +0200

    Package update

--- .emacs.d/elpa/ecukes-20131029.1755/ecukes-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "ecukes" "20131029.1755" "Cucumber for Emacs. [github]"
-  '((f "0.11.0")
-    (s "1.8.0")
-    (dash "2.2.0")
-    (ansi "0.3.0")
-    (espuds "0.2.2")
-    (commander "0.5.1")))
-
--- .emacs.d/elpa/twittering-mode-20130616.1818/twittering-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; twittering-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (twit) "twittering-mode" "twittering-mode.el" (20965
-;;;;;;  20871 203564 842000))
-;;; Generated autoloads from twittering-mode.el
-
-(autoload 'twit "twittering-mode" "\
-Start twittering-mode.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("twittering-mode-pkg.el") (20965 20871
-;;;;;;  368717 900000))
-
-;;;***
-
-(provide 'twittering-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; twittering-mode-autoloads.el ends here
