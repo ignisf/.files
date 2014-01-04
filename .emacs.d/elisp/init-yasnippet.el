854b93962b9580326628ec54948f1b1cd6618df6
Petko Bordjukov <bordjukov@gmail.com>
2014-01-05 01:29:33 +0200

    Update yasnippet snippet dir

--- .emacs.d/elisp/init-yasnippet.el
+++ .emacs.d/elisp/init-yasnippet.el
@@ -1,5 +1,5 @@
 (add-to-list 'load-path
               "~/.emacs.d/vendor/yasnippet")
 (require 'yasnippet)
-(setq yas-snippet-dirs '("~/.emacs.d/vendor/yasnippet/extras/imported"))
+(setq yas-snippet-dirs '("~/.emacs.d/vendor/yasnippet/yasmate/snippets"))
 (yas-global-mode 1)
