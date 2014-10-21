efc1a07371fda904fccbe8afba6b16977060e7c1
Petko Bordjukov <bordjukov@gmail.com>
2014-10-21 15:39:11 +0300

    add rbenv

--- .emacs.d/elisp/init-ruby.el
+++ .emacs.d/elisp/init-ruby.el
@@ -7,6 +7,9 @@
 (add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.jbuilder$" . ruby-mode))
 
+(global-rbenv-mode)
+(rbenv-use-global)
+
 (global-rinari-mode)
 
 (add-hook 'ruby-mode-hook 'robe-mode)
