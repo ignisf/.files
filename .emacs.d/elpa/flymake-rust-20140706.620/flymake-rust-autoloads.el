edf725bb00c0bf499cf341cd0b880b1558c8c43b
Petko Bordjukov <bordjukov@gmail.com>
2014-10-21 17:15:01 +0300

    Update all packages

--- .emacs.d/elpa/flymake-rust-20140706.620/flymake-rust-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; flymake-rust-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("flymake-rust-pkg.el" "flymake-rust.el")
-;;;;;;  (21456 50392 538698 512000))
-
-;;;***
-
-(provide 'flymake-rust-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; flymake-rust-autoloads.el ends here
